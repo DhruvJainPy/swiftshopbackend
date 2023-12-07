ad26ba8638c6b783a7a6432d4654259660f64421
Dhruv Jain <jdhruv175@gmail.com>
2023-12-07 18:03:32 +0530

    Final Commit!

--- server.js
+++ server.js
@@ -33,6 +33,10 @@ app.use(
     saveUninitialized: true,
     cookie: {
       maxAge: 24 * 60 * 60 * 1000,
+      cookie: {
+          sameSite: process.env.NODE_ENV === "production" ? 'none' : 'lax', // must be 'none' to enable cross-site delivery
+          secure: process.env.NODE_ENV === "production", // must be true if sameSite='none'
+        }
     },
   })
 );
